2b647078575f9b930415ad58a54349f41dc57407
Malli <pasupuletinagamalleswararao444@gmail.com>
2026-07-25 12:40:40 +0530

    Time: 0 ms (100%), Space: 42.2 MB (39.12%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long l = 1;
+        long r = num;
+
+        while(l <= r){
+            long mid = l + (r - l) / 2;
+            long sq = mid * mid;
+
+            if(sq == num){
+                return true;
+            }else if(num < sq){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return false;
+    }
+}
