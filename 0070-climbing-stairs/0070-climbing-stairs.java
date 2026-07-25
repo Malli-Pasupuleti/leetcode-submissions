69cafc360cc47e87197e7099ee28026954636398
Malli <pasupuletinagamalleswararao444@gmail.com>
2026-07-25 11:01:18 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,14 +4,16 @@ class Solution {
             return n;
         }
 
-        int[] dp = new int[n + 2];
+        // optimized approach of DP
 
-        dp[1] = 1;
-        dp[2] = 2;
+        int a = 1;
+        int b = 2;
 
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+            int c = a + b;
+            a = b;
+            b = c;
         }
-        return dp[n];
+        return b;
     }
 }
