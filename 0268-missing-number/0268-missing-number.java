4873dfa657ea9fcb5c84a961ecc73715588b6720
Malli <pasupuletinagamalleswararao444@gmail.com>
2026-07-25 12:35:57 +0530

    Time: 0 ms (100%), Space: 47.2 MB (66.35%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        int sum = len * (len + 1) / 2;
+        int real = 0;
+        for(int i = 0; i < len; i++){
+            real += nums[i];
+        }
+        return sum - real;
+    }
+}
