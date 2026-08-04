cfa21c2614fd1a40a2935645119e644a7200699d
Malli <pasupuletinagamalleswararao444@gmail.com>
2026-08-04 14:51:11 +0530

    Time: 3 ms (81.76%), Space: 46.9 MB (42.16%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,16 +1,21 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         List<Integer> ls = new ArrayList<>();
-        
-        Arrays.sort(nums);
-        int curr = nums[0];
+        HashSet<Integer> hs = new HashSet<>();
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
         for (int num : nums){
-            while (curr < num){
-                ls.add(curr);
-                curr++;
+            hs.add(num);
+            if(num < min) min = num;
+            if(num > max) max = num;
+        }
+
+        for (int i = min + 1; i < max; i++){
+            if(!hs.contains(i)){
+                ls.add(i);
             }
-            curr = num + 1;
         }
 
         return ls;
